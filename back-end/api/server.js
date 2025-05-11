92bc856251744e1a4fe9943b6e31ef21dd27275c
Ursula Diefenbach <diefenbach.ushy@gmail.com>
2025-05-11 16:07:09 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,24 +1,31 @@
 import express from 'express';
 import cors from 'cors'; //Middleware - trata a requisição no meio do caminho
 import { db } from './connect.js';
+import path from 'path';
+
+const _dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 
-app.get('/', (request, response) => {
-  response.send(
-    'Olá,Mundo! Com o --watch náo é mais necessário ficar atualizando o servidor'
-  );
+app.get('/api/', (request, response) => {
+  response.send("Só vamos trabalhar com endpoints '/artistas' e '/songs'");
 });
 
-app.get('/artists', async (request, response) => {
+app.get('/api/artists', async (request, response) => {
   response.send(await db.collection('artists').find({}).toArray());
 });
 
-app.get('/songs', async (request, response) => {
+app.get('/api/songs', async (request, response) => {
   response.send(await db.collection('songs').find({}).toArray());
+});
+
+app.use(express.static(path.join(_dirname, '../front-end/dist')));
+
+app.get(/.*/, async (request, response) => {
+  response.sendFile(path.join(_dirname, '../front-end/dist/index.html'));
 });
 
 app.listen(PORT, () => {
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
+// import 'dotenv/config';
 
-const URL = 'http://localhost:3001';
+/* const { NODE_ENV } = process.env;
+const URL = NODE_ENV === 'development' ? 'http://localhost:3001/api' : '/api'; */
+const URL =
+  import.meta.env.MODE === 'development'
+    ? import.meta.env.VITE_API_URL_DEV
+    : import.meta.env.VITE_API_URL_PROD;
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
